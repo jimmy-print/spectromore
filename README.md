57d2d09553d27b6a4df1d1fec330f324602c850d
jimmy-print <jimmy.wang123@outlook.sg>
2026-04-19 10:05:07 +0800

    use fft

--- README.md
+++ README.md
@@ -11,4 +11,11 @@ Next steps
 
 - Use pixelarray to calculate the pixels of the spectrogram, then blit at once instead of using pygame.draw.rect in a for loop.
 
+"Software live vocoder with monitoring
+
+Spectrogram
+
+For my voice: Ability to take out my own harmonics and pitch correction"
+Use spectrogram to get MIDI notes from recording of piano!
+
 ![alt text](https://raw.githubusercontent.com/jimmy-print/spectromore/refs/heads/main/demo%20with%20sine%20and%20saw%20waves.png)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import pygame
 import numpy as np
@@ -6,22 +6,30 @@ import matplotlib.pyplot as plt
 import colorsys
 import wave
 
+import time
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED   = (255, 0, 0)
-BLUE  = (0, 0, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 CYAN = (0, 255, 255)
 
 D_WIDTH, D_HEIGHT = 1850, 800
 display = pygame.display.set_mode((D_WIDTH, D_HEIGHT))
 
-audiofilename = 'file2amono.wav'
+#audiofilename = 'file2amono.wav'
+audiofilename = 'untitleda.wav'
 with open(audiofilename, 'rb') as f:
     raw_data = f.read()
     hexadecimal_string_data = [hex(n)[2:].zfill(2) for n in raw_data]
 
 with wave.open(audiofilename, 'rb') as wavefile:
     sample_rate = wavefile.getparams().framerate
+
+print()
+print(f'Loading file: {audiofilename}')
+print(f'Sample rate: {sample_rate} hz')
+print()
 
 seconds_to_load = 2
 hzr = sample_rate
@@ -34,9 +42,11 @@ for i in range(len(d)):
     tmp.append(str(d[i]))
     if (i - 1) % 2 == 0:
         strf = ''.join(tmp)
-        signed_int = int.from_bytes(bytes.fromhex(strf), byteorder='little', signed=True)
+        signed_int = int.from_bytes(bytes.fromhex(
+            strf), byteorder='little', signed=True)
         alls.append(signed_int)
         tmp = []
+
 
 def main():
     width = D_WIDTH / hz
@@ -56,34 +66,30 @@ def main():
     def get_freqs_and_amplitudes(hzrrange0, hzrrange1):
         firstsplice = alls[int(hzr*hzrrange0):int(hzr*hzrrange1)]
 
-        def DFT(x):
-            N = len(x)
-            n = np.arange(N)
-            k = n.reshape((N, 1))
-            e = np.exp(-2j * np.pi * k * n / N)
-            X = np.dot(e, x)
-            return X
-
-        X = DFT(firstsplice)
-        N = len(X)
-        n = np.arange(N)
-        T = N/hzr
+        X = np.fft.fft(firstsplice)
+        sample_amt = len(X)
+        n = np.arange(sample_amt)
+        T = sample_amt/hzr
         freq = n/T
-
         freqq = np.delete(freq, 0)
+
         XX = np.delete(X, 0)
+        XXX = np.abs(XX)
 
+        XXXX = np.array_split(XXX, 2)[0]
         freqqq = np.array_split(freqq, 2)[0]
-        XXX = abs(np.array_split(XX, 2)[0])
 
-        return freqqq, XXX
+        print(freqqq)
+        return freqqq, XXXX
 
     starting_time = 0.01
     hzrrange0 = starting_time
-    interval = 0.006
+    interval = 0.005
     freq_n = len(get_freqs_and_amplitudes(hzrrange0, hzrrange0+interval)[0])
     ALL = []
     maxs = []
+
+    A = time.time()
     for _ in range(int((seconds_to_load - hzrrange0)*(1/interval))):
         this = list(get_freqs_and_amplitudes(hzrrange0, hzrrange0+interval))
         maxs.append(max(this[1]))
@@ -94,8 +100,10 @@ def main():
             # insert a 0 value at the highest frequency.
             ALL[-1][0] = np.append(ALL[-1][0], [[ALL[-1][0][-1]]])
             ALL[-1][1] = np.append(ALL[-1][1], [[0]])
-        hzrrange0+=interval
+        hzrrange0 += interval
     assert all(len(ALL[i][0]) for i, _ in enumerate(ALL))
+    B = time.time()
+    print(f'FFT time: {B - A} seconds')
 
     pygame.mixer.init()
     pygame.mixer.music.load(audiofilename)
@@ -108,8 +116,6 @@ def main():
     selected = 0
     while True:
         display.fill(BLACK)
-
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -144,7 +150,7 @@ def main():
 
         if playing:
             seconds_in += (1 / fps)
-        #print(frames, fps, seconds_in)
+
         index += index_change
         width += width_change
 
@@ -155,28 +161,29 @@ def main():
             max_amps = max(maxs)
 
             gap = 1
-            height = 3
+            height = (D_HEIGHT - 200) / len(ALL[0][0])
+            print(height)
 
             for j, (freq, amps) in enumerate(zip(freqs, ampss)):
                 ratio = amps/max_amps
                 hsv = colorsys.hsv_to_rgb(ratio, 0.9, 0.6*ratio+0.3)
                 color = hsv[0] * 255, hsv[1] * 255, hsv[2] * 255
- 
+
                 rect = (
                     ((starting_time+interval*i)*width*hzr),
-                    750 - j * (height+gap), #y
+                    750 - j * (height+gap),  # y
                     interval*width*hzr,
                     height,)
 
                 pygame.draw.rect(display, color, rect)
-        
+
         # Draw selector
         pygame.draw.rect(display, WHITE, ((starting_time + interval*selected)*width*hzr,
                                           750 + 10,
                                           interval*width*hzr,
                                           200))
 
-        print(ALL[selected][0])
+        # print(ALL[selected][0])
 
         playhead = (seconds_in * hzr) * width
         pygame.draw.rect(display, RED, (playhead, 100, 1, 200))
@@ -189,7 +196,7 @@ def main():
         # Display waveform
         for i, all_ in enumerate(alls):
             height = (all_/32767)*D_HEIGHT
-            height/=7
+            height /= 7
             pygame.draw.rect(display, WHITE, (
                 i*width, baseline-height/2, 1, height))
 
@@ -198,4 +205,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
